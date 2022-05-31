c8c4ee877ffc7de4369085480125c4c8e0c3b974
OA40 <80436189+frosetrain@users.noreply.github.com>
2022-05-31 11:37:44 +0800

    refactor/style: 31 May stuff

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 /* Nordic Paint v0.0.0 May 23, 2022 */
 
-// Default values, feel free to change
-UIScale = 1;
-brushColours = [
+// Default constants, feel free to change
+const UIScale = 1;
+const brushColours = [
   "#BF616A",
   "#D08770",
   "#EBCB8B",
@@ -12,17 +12,19 @@ brushColours = [
   "#5E81AC",
   "#B48EAD",
 ];
-UIColours = {
+const UIColours = {
   dark: ["#2E3440", "#3B4252", "#4C566A", "#D8DEE9"],
   light: ["#ECEFF4", "#E5E9F0", "#D8DEE9", "#2E3440"],
 };
+
+let brushColourID, brushSize, UITheme, activeUIColours, paintingBorder;
 
 function drawUI() {
   push();
 
   // Bottom bar
   strokeWeight(0);
-  fill(actualUIColours[1]);
+  fill(activeUIColours[1]);
   rect(0, height - 100 * UIScale, width, 100 * UIScale);
 
   // TODO: Make brushes
@@ -32,33 +34,36 @@ function drawUI() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
+
   // Set the default values
   brushColourID = 0;
   brushSize = 10;
   UITheme = "dark";
-  actualUIColours = UIColours[UITheme];
-  paintingBorder = height - 100 * UIScale
+  activeUIColours = UIColours[UITheme];
+  paintingBorder = height - 100 * UIScale;
 
   // Setting up the canvas
-  background(actualUIColours[0]);
+  background(activeUIColours[0]);
   drawUI();
-  
+
   // Applying the variables to the brush
   stroke(brushColours[brushColourID]);
-  strokeWeight(brushSize)
-  
+  strokeWeight(brushSize);
+
   // The code is ready to go!
 }
 
 function draw() {
-  if (mouseIsPressed && mouseY < paintingBorder && pmouseY < paintingBorder && mouseButton == LEFT) {
-    line(mouseX, mouseY, pmouseX, pmouseY)
+  if (
+    mouseIsPressed &&
+    mouseY < paintingBorder &&
+    pmouseY < paintingBorder &&
+    mouseButton == LEFT
+  ) {
+    line(mouseX, mouseY, pmouseX, pmouseY);
   }
 }
 
-function mousePressed() {
-  
-}
+function mousePressed() {}
 
 // TODO: saveCanvas()
