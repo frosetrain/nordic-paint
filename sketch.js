ae5dcb1b57f65b840cd48c7b639e337120338bf0
frosetrain <frosetrain@github.com>
2022-06-04 14:41:22 +0800

    Add nordic-cayman gh pages theme

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-/* Nordic Paint v0.0.0 May 23, 2022 */
+/* Nordic Paint v0.0.0 June 4, 2022 */
 
 // Default constants, feel free to change
 const UIScale = 1;
@@ -13,21 +13,98 @@ const brushColours = [
   "#B48EAD",
 ];
 const UIColours = {
-  dark: ["#2E3440", "#3B4252", "#4C566A", "#D8DEE9"],
+  dark: ["#2E3440", "#4C566A", "#3B4252", "#D8DEE9"],
   light: ["#ECEFF4", "#E5E9F0", "#D8DEE9", "#2E3440"],
 };
 
-let brushColourID, brushSize, UITheme, activeUIColours, paintingBorder;
+let activeBrushID,
+  brushSize,
+  UITheme,
+  activeUIColours,
+  paintingBorder,
+  activeBrushColours;
 
 function drawUI() {
-  push();
-
   // Bottom bar
+  push();
   strokeWeight(0);
-  fill(activeUIColours[1]);
+  fill(activeUIColours[2]);
   rect(0, height - 100 * UIScale, width, 100 * UIScale);
+  pop();
 
-  // TODO: Make brushes
+  // Brush 0: Pen
+  push();
+  strokeWeight(0);
+
+  if (activeBrushID == 0) {
+    translate(50 * UIScale, height - 80 * UIScale);
+  } else {
+    translate(50 * UIScale, height - 60 * UIScale);
+  }
+
+  fill(brushColours[activeBrushColours[activeBrushID]]);
+  triangle(
+    0,
+    0 * UIScale,
+    5 * UIScale,
+    15 * UIScale,
+    -5 * UIScale,
+    15 * UIScale
+  );
+  fill(activeUIColours[3]);
+  quad(
+    -5 * UIScale,
+    15 * UIScale,
+    5 * UIScale,
+    15 * UIScale,
+    10 * UIScale,
+    30 * UIScale,
+    -10 * UIScale,
+    30 * UIScale
+  );
+  fill(brushColours[activeBrushColours[activeBrushID]]);
+  rect(-10 * UIScale, 30 * UIScale, 20 * UIScale, 5 * UIScale);
+  fill(activeUIColours[3]);
+  rect(-10 * UIScale, 35 * UIScale, 20 * UIScale, 45 * UIScale);
+
+  pop();
+
+  // Brush 1: Stamp
+  push();
+  strokeWeight(0);
+
+  if (activeBrushID == 1) {
+    translate(110 * UIScale, height - 80 * UIScale);
+  } else {
+    translate(110 * UIScale, height - 60 * UIScale);
+  }
+  fill(brushColours[activeBrushColours[activeBrushID]]);
+  rect(-10 * UIScale, 0 * UIScale, 20 * UIScale, 5 * UIScale);
+  fill(activeUIColours[3]);
+  rect(-10 * UIScale, 5 * UIScale, 20 * UIScale, 10 * UIScale);
+  quad(
+    -10 * UIScale,
+    15 * UIScale,
+    10 * UIScale,
+    15 * UIScale,
+    5 * UIScale,
+    20 * UIScale,
+    -5 * UIScale,
+    20 * UIScale
+  );
+  // TODO: Paste a picture of the active stamp shape on the icon
+  rect(-5 * UIScale, 20 * UIScale, 10 * UIScale, 10 * UIScale);
+  quad(
+    -5 * UIScale,
+    30 * UIScale,
+    5 * UIScale,
+    30 * UIScale,
+    10 * UIScale,
+    45 * UIScale,
+    -10 * UIScale,
+    45 * UIScale
+  );
+  rect(-10 * UIScale, 45 * UIScale, 20 * UIScale, 35 * UIScale);
 
   pop();
 }
@@ -35,8 +112,9 @@ function drawUI() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  // Set the default values
-  brushColourID = 0;
+  // Setting the default values
+  activeBrushColours = [0, 0, 0];
+  activeBrushID = 0;
   brushSize = 10;
   UITheme = "dark";
   activeUIColours = UIColours[UITheme];
@@ -44,11 +122,13 @@ function setup() {
 
   // Setting up the canvas
   background(activeUIColours[0]);
-  drawUI();
 
   // Applying the variables to the brush
-  stroke(brushColours[brushColourID]);
+  stroke(brushColours[activeBrushColours[activeBrushID]]);
   strokeWeight(brushSize);
+
+  // Drawing the UI
+  drawUI();
 
   // The code is ready to go!
 }
@@ -60,10 +140,18 @@ function draw() {
     pmouseY < paintingBorder &&
     mouseButton == LEFT
   ) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
+    if (activeBrushID == 0) {
+      line(mouseX, mouseY, pmouseX, pmouseY);
+    } else if (activeBrushID == 1) {
+      circle(mouseX, mouseY, brushSize / 2)
+    }
   }
 }
 
-function mousePressed() {}
+function mousePressed() {
+  if (mouseX >= 20 && mouseX <= 120 && mouseY >= height - 80) {
+    activeBrushID = Math.round((mouseX - 50) / 60);
+    drawUI()
+  }
+}
 
-// TODO: saveCanvas()
