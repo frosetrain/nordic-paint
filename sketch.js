18dbd671f4db78b7c36e5da2045ba9ba897cb231
Frosetrain <80436189+frosetrain@users.noreply.github.com>
2022-06-17 15:30:23 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -133,10 +133,6 @@ function setup() {
   // The code is ready to go!
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth - 80, windowHeight - 80);
-}
-
 function draw() {
   if (
     mouseIsPressed &&
