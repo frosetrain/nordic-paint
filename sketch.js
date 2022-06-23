7b7f5dcb07132c08b6849f32823ed6ea9f0fb207
Frosetrain <80436189+frosetrain@users.noreply.github.com>
2022-06-23 19:17:23 +0800

    Add side bar with brushes + add colour selectors

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-/* Nordic Paint v0.0.0 June 4, 2022 */
+/* Nordic Paint v0.0.0 June 23, 2022 */
 
 // Default constants, feel free to change
 const UIScale = 1;
@@ -17,14 +17,22 @@ const UIColours = {
   light: ["#ECEFF4", "#E5E9F0", "#D8DEE9", "#2E3440"],
 };
 
-let activeBrushID,
-  brushSize,
-  UITheme,
-  activeUIColours,
-  paintingBorder,
-  activeBrushColours;
+let activeBrushID, UITheme, activeUIColours, activeBrushColours, brushSizes;
 
 function drawUI() {
+  // Side bar
+  push();
+  strokeWeight(0);
+  fill(activeUIColours[2]);
+  rect(
+    width - 100 * UIScale,
+    50 * UIScale,
+    110 * UIScale,
+    160 * UIScale,
+    10 * UIScale
+  );
+  pop();
+
   // Bottom bar
   push();
   strokeWeight(0);
@@ -35,77 +43,78 @@ function drawUI() {
   // Brush 0: Pen
   push();
   strokeWeight(0);
-
   if (activeBrushID == 0) {
-    translate(50 * UIScale, height - 80 * UIScale);
+    translate(width - 80 * UIScale, 100 * UIScale);
   } else {
-    translate(50 * UIScale, height - 60 * UIScale);
+    translate(width - 60 * UIScale, 100 * UIScale);
   }
-
   fill(brushColours[activeBrushColours[activeBrushID]]);
-  triangle(
-    0,
-    0 * UIScale,
-    5 * UIScale,
-    15 * UIScale,
-    -5 * UIScale,
-    15 * UIScale
-  );
+  triangle(0, 0, 15 * UIScale, 5 * UIScale, 15 * UIScale, -5 * UIScale);
   fill(activeUIColours[3]);
   quad(
+    15 * UIScale,
     -5 * UIScale,
     15 * UIScale,
     5 * UIScale,
-    15 * UIScale,
+    30 * UIScale,
     10 * UIScale,
     30 * UIScale,
-    -10 * UIScale,
-    30 * UIScale
+    -10 * UIScale
   );
   fill(brushColours[activeBrushColours[activeBrushID]]);
-  rect(-10 * UIScale, 30 * UIScale, 20 * UIScale, 5 * UIScale);
+  rect(30 * UIScale, -10 * UIScale, 5 * UIScale, 20 * UIScale);
   fill(activeUIColours[3]);
-  rect(-10 * UIScale, 35 * UIScale, 20 * UIScale, 45 * UIScale);
-
+  rect(35 * UIScale, -10 * UIScale, 45 * UIScale, 20 * UIScale);
   pop();
 
   // Brush 1: Stamp
   push();
   strokeWeight(0);
-
   if (activeBrushID == 1) {
-    translate(110 * UIScale, height - 80 * UIScale);
+    translate(width - 80 * UIScale, 160 * UIScale);
   } else {
-    translate(110 * UIScale, height - 60 * UIScale);
+    translate(width - 60 * UIScale, 160 * UIScale);
   }
   fill(brushColours[activeBrushColours[activeBrushID]]);
-  rect(-10 * UIScale, 0 * UIScale, 20 * UIScale, 5 * UIScale);
+  rect(0 * UIScale, -10 * UIScale, 5 * UIScale, 20 * UIScale);
   fill(activeUIColours[3]);
-  rect(-10 * UIScale, 5 * UIScale, 20 * UIScale, 10 * UIScale);
+  rect(5 * UIScale, -10 * UIScale, 10 * UIScale, 20 * UIScale);
   quad(
+    15 * UIScale,
     -10 * UIScale,
     15 * UIScale,
     10 * UIScale,
-    15 * UIScale,
+    20 * UIScale,
     5 * UIScale,
     20 * UIScale,
-    -5 * UIScale,
-    20 * UIScale
+    -5 * UIScale
   );
   // TODO: Paste a picture of the active stamp shape on the icon
-  rect(-5 * UIScale, 20 * UIScale, 10 * UIScale, 10 * UIScale);
+  rect(20 * UIScale, -5 * UIScale, 10 * UIScale, 10 * UIScale);
   quad(
+    30 * UIScale,
     -5 * UIScale,
     30 * UIScale,
     5 * UIScale,
-    30 * UIScale,
+    45 * UIScale,
     10 * UIScale,
     45 * UIScale,
-    -10 * UIScale,
-    45 * UIScale
+    -10 * UIScale
   );
-  rect(-10 * UIScale, 45 * UIScale, 20 * UIScale, 35 * UIScale);
+  rect(45 * UIScale, -10 * UIScale, 35 * UIScale, 20 * UIScale);
+  pop();
 
+  // Colour selectors
+  push();
+  strokeWeight(0);
+  for (i = 0; i < 8; i++) {
+    fill(brushColours[i]);
+    circle(
+      (i % 4) * 30 + 30 * UIScale,
+      height - 30 * Math.floor(i / 4) - 35 * UIScale,
+      24 * UIScale
+    );
+  }
   pop();
 }
 
@@ -115,17 +124,18 @@ function setup() {
   // Setting the default values
   activeBrushColours = [0, 0, 0];
   activeBrushID = 0;
-  brushSize = 10;
+  brushSizes = [10, 10];
   UITheme = "dark";
   activeUIColours = UIColours[UITheme];
-  paintingBorder = height - 100 * UIScale;
 
   // Setting up the canvas
-  background(activeUIColours[0]);
+  fill(activeUIColours[0]);
+  strokeWeight(0);
+  rect(0, 0, width, height, 10);
 
   // Applying the variables to the brush
   stroke(brushColours[activeBrushColours[activeBrushID]]);
-  strokeWeight(brushSize);
+  strokeWeight(brushSizes[activeBrushID]);
 
   // Drawing the UI
   drawUI();
@@ -136,21 +146,34 @@ function setup() {
 function draw() {
   if (
     mouseIsPressed &&
-    mouseY < paintingBorder &&
-    pmouseY < paintingBorder &&
+    mouseY < height - 100 * UIScale &&
+    pmouseY < height - 100 * UIScale &&
+    mouseX < width - 100 &&
+    pmouseX < width - 100 &&
     mouseButton == LEFT
   ) {
     if (activeBrushID == 0) {
       line(mouseX, mouseY, pmouseX, pmouseY);
     } else if (activeBrushID == 1) {
-      circle(mouseX, mouseY, brushSize / 2)
+      circle(mouseX, mouseY, brushSizes[1]);
     }
   }
 }
 
 function mousePressed() {
-  if (mouseX >= 20 && mouseX <= 120 && mouseY >= height - 80) {
-    activeBrushID = Math.round((mouseX - 50) / 60);
-    drawUI()
+  if (
+    mouseX >= width - 80 * UIScale &&
+    mouseY >= 80 * UIScale &&
+    mouseY <= 180 * UIScale
+  ) {
+    activeBrushID = Math.round((mouseY - 100) / 60);
+    drawUI();
+    if (activeBrushID == 0) {
+      stroke(brushColours[activeBrushColours[activeBrushID]]);
+      strokeWeight(brushSizes[activeBrushID]);
+    } else if (activeBrushID == 1) {
+      strokeWeight(0);
+      fill(brushColours[activeBrushColours[activeBrushID]]);
+    }
   }
 }
