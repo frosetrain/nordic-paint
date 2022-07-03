69548f67ebae51a88ce2565040bcb9c0219b6b98
frosetrain <frosetrain@github.com>
2022-07-03 20:50:35 +0800

    i forgot what i did lol

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-/* Nordic Paint v0.0.0 June 25, 2022 */
+/* Nordic Paint v0.0.0 July 3, 2022 */
 
 // Default constants, feel free to change
 const brushColours = [
@@ -15,7 +15,8 @@ const UIColours = {
   dark: ["#2E3440", "#4C566A", "#3B4252", "#D8DEE9"],
   light: ["#ECEFF4", "#E5E9F0", "#D8DEE9", "#2E3440"],
 };
-const brushNames = ["Pen", "Stamp", "Path", "Eraser"]
+const brushNames = ["Pen", "Stamp", "Path", "Eraser"];
+const brushKeybinds = ["q", "w", "e", "r"];
 
 let activeBrushID,
   UITheme,
@@ -26,33 +27,10 @@ let activeBrushID,
   previousClickX,
   previousClickY;
 
-function star(x, y, diameter) {
-  noLoop();
-  let npoints = 5;
-  let radius1 = diameter / 2;
-  let radius2 = radius1 * 0.3819660113;
-  let angle = TWO_PI / npoints;
-  let halfAngle = angle / 2.0;
-  push();
-  translate(width * (x / width), height * (y / height));
-  x = 0;
-  y = 0;
-  rotate(PI / 10);
-  beginShape();
-  for (let a = 0; a < TWO_PI; a += angle) {
-    let sx = x + cos(a) * radius2;
-    let sy = y + sin(a) * radius2;
-    vertex(sx, sy);
-    sx = x + cos(a + halfAngle) * radius1;
-    sy = y + sin(a + halfAngle) * radius1;
-    vertex(sx, sy);
-  }
-  endShape(CLOSE);
-  pop();
-}
-
 function preload() {
-  rickImage = loadImage('assets/images/rick.jpg');
+  rickImage = loadImage(
+    "https://raw.githubusercontent.com/frosetrain/nordic-paint/main/assets/images/rick.jpg"
+  );
 }
 
 function drawUI() {
@@ -61,17 +39,17 @@ function drawUI() {
   strokeWeight(0);
   fill(activeUIColours[2]);
   rect(0, 0, width, 50, 10);
-  fill(activeUIColours[0])
+  fill(activeUIColours[0]);
   rect(0, 40, width, 10);
   fill(activeUIColours[3]);
-  textAlign(CENTER)
-  textFont("Inter")
-  textSize(16)
-  text("Nordic Paint", width / 2, 26)
-  fill(activeUIColours[1])
-  circle(width - 20, 20, 25)
-  fill(activeUIColours[3])
-  text("?",  width - 20, 25)
+  textAlign(CENTER);
+  textFont("Inter");
+  textSize(16);
+  text("Nordic Paint", width / 2, 26);
+  fill(activeUIColours[1]);
+  circle(width - 20, 20, 25);
+  fill(activeUIColours[3]);
+  text("?", width - 20, 25);
   pop();
 
   // Side bar
@@ -80,12 +58,12 @@ function drawUI() {
   fill(activeUIColours[2]);
   rect(width - 100, 50, 110, 250, 10);
   fill(activeUIColours[0]);
-  rect(width - 100, 300, 100, 25)
-  textAlign(CENTER)
-  textFont("Inter")
-  textSize(15)
-  fill(activeUIColours[3])
-  text(brushNames[activeBrushID], width - 50, 320)
+  rect(width - 100, 300, 100, 25);
+  textAlign(CENTER);
+  textFont("Inter");
+  textSize(15);
+  fill(activeUIColours[3]);
+  text(brushNames[activeBrushID], width - 50, 320);
   pop();
 
   // Bottom bar
@@ -168,17 +146,17 @@ function drawUI() {
 
   // Colour selectors
   if (activeBrushID != 3) {
-   push();
-   strokeWeight(0);
+    push();
+    strokeWeight(0);
     for (i = 0; i < 8; i++) {
       fill(brushColours[i]);
-     circle((i % 4) * 30 + 30, height - 30 * Math.floor(i / 4) - 35, 24);
+      circle((i % 4) * 30 + 30, height - 30 * Math.floor(i / 4) - 35, 24);
     }
     strokeWeight(3);
     stroke(activeUIColours[0]);
     fill(0, 0, 0, 0);
     circle(
-     (activeBrushColours[activeBrushID] % 4) * 30 + 30,
+      (activeBrushColours[activeBrushID] % 4) * 30 + 30,
       height - 30 * Math.floor(activeBrushColours[activeBrushID] / 4) - 35,
       16
     );
@@ -190,7 +168,7 @@ function drawUI() {
   if (activeBrushID != 3) {
     translate(160, 0);
   } else {
-    translate(20, 0)
+    translate(20, 0);
   }
   strokeWeight(0);
   fill(activeUIColours[1]);
@@ -209,9 +187,9 @@ function drawUI() {
 
   // Stamp shape chooser
   if (activeBrushID === 1) {
-    push()
-    translate(280, 0)
-    strokeWeight(0)
+    push();
+    translate(280, 0);
+    strokeWeight(0);
     fill(activeUIColours[1]);
     circle(15, height - 38, 20);
     rect(30, height - 50, 40, 24, 2);
@@ -225,13 +203,51 @@ function drawUI() {
     text(">", 85, height - 33);
     switch (stampShape) {
       case 0:
-        circle(50, height - 38, 13)
+        circle(50, height - 38, 13);
         break;
       case 1:
-        circle()
+        triangle(
+          50,
+          height - 43,
+          50 - 13 * 0.6,
+          height - 30,
+          50 + 13 * 0.6,
+          height - 30
+        );
+        break;
+      case 2:
+        square(44, height - 44, 13);
+        break;
+      case 3:
+        image(
+          rickImage,
+          44,
+          height - 44,
+          13,
+          13
+        );
+        break;
     }
-    pop()
+    pop();
   }
+
+  // Clear canvas button
+  push()
+  strokeWeight(0)
+  fill(activeUIColours[1])
+  rect(width - 120, height - 80, 100, 25)
+  rect(width - 120, height - 45, 100, 25)
+  fill(activeUIColours[3])
+  textAlign(CENTER)
+  textFont("Inter")
+  textSize(16)
+  text("Clear", width - 70, height - 62)
+  if (UITheme === "dark") {
+    text("Light Mode", width - 70, height - 28)
+  } else if (UITheme === "light") {
+    text("Dark Mode", width - 70, height - 28)
+  }
+  pop()
 
   // Setting the variables (yes this isn't really part of "drawing UI")
   switch (activeBrushID) {
@@ -263,7 +279,7 @@ function setup() {
   brushSizes = [10, 10, 10, 10, 10];
   UITheme = "dark";
   activeUIColours = UIColours[UITheme];
-  stampShape = 0
+  stampShape = 0;
 
   // Creating the canvas
   createCanvas(windowWidth - 80, windowHeight - 80);
@@ -295,14 +311,29 @@ function draw() {
         switch (stampShape) {
           case 0:
             circle(mouseX, mouseY, brushSizes[activeBrushID]);
+            break;
           case 1:
-            triangle(mouseX, mouseY - brushSizes[activeBrushID], mouseX - brushSizes[activeBrushID] * 0.6, mouseY, mouseX + brushSizes[activeBrushID] * 0.6, mouseY)
+            triangle(
+              mouseX,
+              mouseY - brushSizes[activeBrushID],
+              mouseX - brushSizes[activeBrushID] * 0.6,
+              mouseY,
+              mouseX + brushSizes[activeBrushID] * 0.6,
+              mouseY
+            );
+            break;
           case 2:
-            square(mouseX, mouseY, brushSizes[activeBrushID])
+            square(mouseX, mouseY, brushSizes[activeBrushID]);
+            break;
           case 3:
-            star(mouseX, mouseY, brushSizes[activeBrushID])
-          case 4:
-            image(rickImage, mouseX, mouseY, brushSizes[activeBrushID], brushSizes[activeBrushID])
+            image(
+              rickImage,
+              mouseX,
+              mouseY,
+              brushSizes[activeBrushID],
+              brushSizes[activeBrushID]
+            );
+            break;
         }
         break;
       // case 2 isn't here because it's handled in mousePressed()
@@ -356,7 +387,7 @@ function mousePressed() {
     }
     drawUI();
   }
-  
+
   // Size changing button
   if (activeBrushID != 3) {
     if (
@@ -365,8 +396,7 @@ function mousePressed() {
     ) {
       brushSizes[activeBrushID]--;
       drawUI();
-    }
-    else if (
+    } else if (
       dist(mouseX, mouseY, 245, height - 38) <= 20 &&
       brushSizes[activeBrushID] < 100
     ) {
@@ -380,8 +410,7 @@ function mousePressed() {
     ) {
       brushSizes[activeBrushID]--;
       drawUI();
-    }
-    else if (
+    } else if (
       dist(mouseX, mouseY, 105, height - 38) <= 20 &&
       brushSizes[activeBrushID] < 100
     ) {
@@ -392,20 +421,32 @@ function mousePressed() {
 
   // Stamp changing button
   if (activeBrushID === 1) {
-    if (
-      dist(mouseX, mouseY, 295, height - 38) <= 20 &&
-      stampShape > 0
-    ) {
+    if (dist(mouseX, mouseY, 295, height - 38) <= 20 && stampShape > 0) {
       stampShape--;
+      console.log(stampShape);
       drawUI();
-    }
-    else if (
-      dist(mouseX, mouseY, 365, height - 38) <= 20 &&
-      stampShape < 4
-    ) {
+    } else if (dist(mouseX, mouseY, 365, height - 38) <= 20 && stampShape < 3) {
       stampShape++;
+      console.log(stampShape);
       drawUI();
     }
   }
-  
+}
+
+function keyPressed() {
+  console.log(key)
+  if (parseInt(key) >= 1 && parseInt(key) <= 8) {
+    activeBrushColours[activeBrushID] = parseInt(key) - 1
+    drawUI();
+  } else if (brushKeybinds.includes(key)) {
+    activeBrushID = brushKeybinds.indexOf(key)
+    drawUI();
+  } else if (key === "?") {
+    window.open("https://frosetrain.github.io/nordic-paint/documentation");
+  } else if (key === "Backspace") {
+    fill(activeUIColours[0]);
+    strokeWeight(0);
+    rect(0, 0, width, height, 10);
+    drawUI()
+  }
 }
